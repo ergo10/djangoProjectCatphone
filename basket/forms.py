ccbd50423dd03f3227a0004d8f6cb36a9a7e9958
ergo10 <123301613+ergo10@users.noreply.github.com>
2023-03-25 14:23:23 +0300

    Delete basket directory

--- basket/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-PROD_MAX_COUNT = [(i, str(i)) for i in range(1, 25)]
-
-
-class BasketAddProductForm(forms.Form):
-    count_prod = forms.TypedChoiceField(choices=PROD_MAX_COUNT, coerce=int, label='Количество')
-    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
